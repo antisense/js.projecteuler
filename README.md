b9503d750f3307f1f37dcdb66d4563939af8b569
Adam Lecuyer <adam.lecuyer@gmail.com>
2015-01-25 09:54:04 -0500

    Problem 1 solved

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # js.projecteuler
+These are three problems asked for from The Odin Project
---
+++ problem1.js
@@ -0,0 +1,10 @@
+var sum = 0;
+var i = 0;
+
+while (i<1000) {
+	if (i % 3 === 0) or (i % 5 === 0) {
+		sum += i;
+	}
+	i++;
+}
+console.log(sum);
